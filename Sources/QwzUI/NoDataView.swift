37f7248df2ef8c8fe23c4039cc8e001f33f4100d
DavidQin <david.qin@harman.com>
2026-02-26 14:20:48 +0800

    Fix init func

--- Sources/QwzUI/NoDataView.swift
+++ Sources/QwzUI/NoDataView.swift
@@ -8,18 +8,30 @@
 import SwiftUI
 
 public struct NoDataView: View {
-    public var label: String? = nil
+    public var text: String?
+    public var image: Image
+    public var foregroundColor: Color
+    
+    public init(
+        text: String? = nil,
+        image: Image? = nil,
+        foregroundColor: Color? = nil
+    ) {
+        self.text = text
+        self.image = image ?? Image(systemName: "tray")
+        self.foregroundColor = foregroundColor ?? Color.qwz.textSecondary
+    }
     
     public var body: some View {
         VStack {
-            Image(systemName: "tray")
+            image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40, height: 40)
                 .foregroundStyle(Color.qwz.textSecondary)
             
-            if let label {
-                Text(label)
+            if let text {
+                Text(text)
                     .font(.body)
                     .foregroundStyle(Color.qwz.textSecondary)
             }
